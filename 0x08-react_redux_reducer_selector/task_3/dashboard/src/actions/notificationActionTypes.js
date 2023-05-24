cf9f13981159b9e2c9d99c8dea3d34ec9b816bb1
teslynnchristians <104847651+teslynnchristians@users.noreply.github.com>
2023-05-24 13:29:40 +0200

    React Redux reducer+selector

--- 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionTypes.js
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/actions/notificationActionTypes.js
@@ -2,6 +2,6 @@ export const MARK_AS_READ = 'MARK_AS_READ';
 export const SET_TYPE_FILTER = 'SET_TYPE_FILTER';
 
 export const NotificationTypeFilters = {
-    DEFAULT: 'DEFAULT',
-    URGENT: 'URGENT'
-}
+  DEFAULT: 'DEFAULT',
+  URGENT: 'URGENT',
+};
